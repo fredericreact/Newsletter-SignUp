7a329a7886757e18a0b54f37b95eb83339eccc7e
unknown <fredericreact@gmail.com>
2022-06-11 12:31:35 +0800

    setup heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const request = require("request");
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended:true}));
 
