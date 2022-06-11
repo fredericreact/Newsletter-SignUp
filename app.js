ff27a58b9776300976d57efed870ebf7bfc0a7e0
unknown <fredericreact@gmail.com>
2022-06-11 12:26:36 +0800

    add success failure pages

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ const options ={
  }
 
  const request = https.request(url, options, function(response){
+
+   if (response.statusCode === 200 ){
+       res.sendFile(__dirname+"/success.html");
+     } else {
+       res.sendFile(__dirname+"/failure.html");
+     }
+
+
 response.on("data",function(data){
   console.log(JSON.parse(data));
 })
@@ -54,6 +62,9 @@ request.end();
 
 })
 
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
 
 app.listen(port, function () {
   console.log("listening port 3000");
