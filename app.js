1ff54fe25ca2c4d79e8a4da6cbb875721de982f9
unknown <fredericreact@gmail.com>
2022-06-11 11:52:13 +0800

    adjust html and css and send file

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const request = require("request");
 const app = express()
 const port = 3000
 
+
+
+app.get("/",function(req,res){
+
+  res.sendFile(__dirname+"/signup.html");
+})
+
 app.listen(port, function () {
   console.log("listening port 3000");
 });
